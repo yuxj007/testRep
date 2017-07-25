816b5d07fbab7a168226be2b608bd13221e76941
yxjmac <yxjmac@localhost>
2017-07-25 14:00:03 +0800

    mm

---
+++ src/main/java/test/autotest/App.java
@@ -0,0 +1,13 @@
+package test.autotest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
