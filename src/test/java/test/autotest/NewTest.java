549ce6f731b7ebda342f3e0d28bcaa00665a382f
yuxj007 <yuxiujuan_001@163.com>
2017-08-09 12:21:45 +0800

    add NewTest

---
+++ src/test/java/test/autotest/NewTest.java
@@ -0,0 +1,18 @@
+package test.autotest;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+
+public class NewTest {
+  @Test
+  public void testcase1() throws Exception {
+
+	
+	  		String mid="99bc4c61-507e-4bd6-8b97-4274795ed73";
+	  		String url="http://grcdemo.smartdot.com:38080/demo-web/rest/permission/menuResource/user/1797852098989738004/application/DEFAULT/firstLevelMenus";
+	  		System.out.println("******Test Case2:	查询状态接口（Get请求）:"+url);
+	  		Assert.assertTrue("HTTP response body contains mid or not:", RestAPITest.setUPGET(url));
+	  	
+	  	}
+}
